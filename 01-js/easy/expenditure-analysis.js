409ca87a69b44cbe2dc55892526c22fd4f1501e0
sujit <sujitshrestha0004@gmail.com>
2023-06-15 10:16:09 +0545

    completed but test failed for some reason while checking manually the tests passes

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -9,7 +9,50 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  let output = [];
+ 
+  if(Object.keys(transactions).length === 0){
+    return output;
+  }
+  
+  for(let i=0 ; i<6 ; i++){
+      let cat = {};
+      let catName = "";
+      catName = transactions[i].category;
+      let catPrice = 0;
+      
+      
+      for(let j=0 ; j<6 ; j++){
+        if(transactions[j].category == catName){
+          catPrice = catPrice + transactions[j].price;
+      
+        }
+      }   
+      cat[catName]  = catPrice;
+      output.push(cat);
+     
+  }
+
+  const newOutput = output.filter(
+    (obj , index , self) =>
+    index ===
+    self.findIndex(
+      (o) => JSON.stringify(o) === JSON.stringify(obj)
+    )
+  );
+
+
+  return newOutput;
 }
+
+let transactions = [{ itemName : "milk" ,  category : "food", price : 120 ,  timestamp : 1} ,
+                    { itemName : "carrots" ,  category : "food", price : 50 ,  timestamp : 1}, 
+                    { itemName : "keyboard" ,category : "electronics", price : 5000 ,timestamp : 1},
+                    { itemName : "monitor" ,  category : "electronics", price : 27000 ,  timestamp : 1} ,
+                    { itemName : "shirt" ,  category : "clothes", price : 575 ,  timestamp : 1}, 
+                    { itemName : "pants" ,  category : "food", price : 1300 ,  timestamp : 1}
+                   ];
+const ans = calculateTotalSpentByCategory(transactions);
+console.log(ans);
 
 module.exports = calculateTotalSpentByCategory;
